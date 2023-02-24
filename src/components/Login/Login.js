9e72e50c06d3a35d8715bf9d79b1a96a3e3a1c29
shibaftw2022 <shibaftw2022@gmail.com>
2023-02-24 16:51:22 +0800

    stop at lesson 122

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -26,7 +26,7 @@ const passwordReducer = (state, action) =>{
   if(action.type === 'INPUT_BLUR')
   {
     return {value: state.value, isValid : state.value.trim().length > 6};
-  }
+  } 
 
   return {value: '', isValid : null};
 
